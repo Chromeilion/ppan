1d9b7dad1f4f4468d8ed14cd3c53de4f87e861a2
chromeilion <uki@cro.moe>
2024-07-29 13:28:13 +0200

    add shebang to run_ppan.sh

--- scripts/run_ppan.sh
+++ scripts/run_ppan.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 #SBATCH --partition=GPU
 #SBATCH --job-name=bw_test
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=24
-#SBATCH --time=06:00:00
+#SBATCH --time=02:00:00
 #SBATCH --output=./logs/run%j.out
 #SBATCH --exclusive
 
