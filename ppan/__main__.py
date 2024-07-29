50656082fd276faa0ff19c02bbbfc59cf7af33e1
chromeilion <uki@cro.moe>
2024-07-29 13:27:39 +0200

    fix environment variables in main and example

--- ppan/__main__.py
+++ ppan/__main__.py
@@ -177,7 +177,7 @@ def main():
         "-r", "--checkpoint-dir",
         action="store",
         help="Location of a training checkpoint when continuing training.",
-        default=os.environ.get("PPAN_PRETRAIN_MODEL_CHECKPOINT", None),
+        default=os.environ.get("PPAN_OUTPUT_DIR", None),
         required=False,
     )
     parser_pretrain.add_argument(
