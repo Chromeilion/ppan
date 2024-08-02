481edc5954c5e6176b0e184655a01416822f1756
Chromeilion <uki@cro.moe>
2024-08-02 13:24:29 +0200

    update env var name

--- ppan/__main__.py
+++ ppan/__main__.py
@@ -169,7 +169,7 @@ def main():
     parser_pretrain.add_argument(
         "-o", "--output-dir",
         action="store",
-        default=os.environ.get("PPAN_OUTPUT_DIR"),
+        default=os.environ.get("PPAN_PRETRAIN_OUTPUT_DIR"),
         help="Where to save the trained model, full path with filename.",
         required=False,
     )
